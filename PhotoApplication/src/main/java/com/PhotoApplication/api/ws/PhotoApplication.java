7e0c74680fb9224504ee512c541ab523f2e42cb0
saikrishna434343 <60270778+saikrishna434343@users.noreply.github.com>
2020-05-21 17:41:31 -0500

    inital setup

---
+++ PhotoApplication/src/main/java/com/PhotoApplication/api/ws/PhotoApplication.java
@@ -0,0 +1,16 @@
+package com.PhotoApplication.api.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoApplication.class, args);
+	}
+
+}
